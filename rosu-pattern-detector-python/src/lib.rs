2aecb5685907f2b262c9daeac87f20663ced4e9b
iamnotcoding <shw42909@gmail.com>
2025-05-26 04:39:18 +0900

    revert lib.rs

--- rosu-pattern-detector-python/src/lib.rs
+++ rosu-pattern-detector-python/src/lib.rs
@@ -1,14 +1,11 @@
 use pyo3::prelude::*;
 use rosu_pattern_detector::calc::get_patterns;
-use serde_json;
 
 #[pyfunction]
 fn get_patterns_python(path: String) -> PyResult<String> {
     let patterns = get_patterns(&path)
         .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
-    let json = serde_json::to_string(&patterns)
-        .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
-    Ok(json)
+    Ok(format!("{:?}", patterns))
 }
 
 /// A Python module implemented in Rust.
