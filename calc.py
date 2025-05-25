a67dc7de0ee8032d06ca56570a28b41cb7432adf
iamnotcoding <shw42909@gmail.com>
2025-05-26 04:36:38 +0900

    modified output as the example on the github rosu-pattern-detector

--- calc.py
+++ calc.py
@@ -1,8 +1,35 @@
 import rosu_pattern_detector_python
+import json
+
+def parse(s : str) -> dict:
+    result = {}
+
+    for line in s[1:-1].split(','):
+        key, value = line.split(':', 1)
+        result[key.strip()] = float(value.strip())
+
+    return result
+
+def aggregate_patterns(patterns: dict) -> dict:
+    result = {}
+
+    for k, v in patterns.items():
+        main_type = k.split('(')[0].lower()
+
+        if main_type == 'none':
+            continue
+
+        if main_type not in result:
+            result[main_type] = 0.0
+
+        result[main_type] += v
+    return result
 
 def get_pattern_stats(osu_file_path:str):
-    return rosu_pattern_detector_python.get_patterns_python(osu_file_path)
-
+    raw_string =  rosu_pattern_detector_python.get_patterns_python(osu_file_path)
+    raw_dic = parse(raw_string)
+    aggreated_dic = aggregate_patterns(raw_dic)
+    return aggreated_dic
 
 if __name__ == "__main__":
     print(get_pattern_stats(r"C:\Users\shw42\source\maina-skill-stats\Kurokotei - Galaxy Collapse (Mat) [Cataclysmic Hypernova].osu"))
--- rosu-pattern-detector-python/src/lib.rs
+++ rosu-pattern-detector-python/src/lib.rs
@@ -1,11 +1,14 @@
 use pyo3::prelude::*;
 use rosu_pattern_detector::calc::get_patterns;
+use serde_json;
 
 #[pyfunction]
 fn get_patterns_python(path: String) -> PyResult<String> {
     let patterns = get_patterns(&path)
         .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
-    Ok(format!("{:?}", patterns))
+    let json = serde_json::to_string(&patterns)
+        .map_err(|e| pyo3::exceptions::PyValueError::new_err(e.to_string()))?;
+    Ok(json)
 }
 
 /// A Python module implemented in Rust.
